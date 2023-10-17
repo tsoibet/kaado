96a6c43ef976078d8394ac960222e0629fb62d6e
Nasu Don <hck.hongkong@gmail.com>
2023-10-17 18:17:56 +0900

    KAADO-30: - Fix format

--- src/services/shareService.ts
+++ src/services/shareService.ts
@@ -30,11 +30,11 @@ export async function getSharings() {
     }
 }
 
-export async function createSharing(cardId: string, validHours = (24*7)) {
+export async function createSharing(cardId: string, validHours = 24 * 7) {
     try {
         await connectDB();
-        
-        const {card} = await getCard(cardId);
+
+        const { card } = await getCard(cardId);
 
         if (!card) {
             return { error: 'card not found' };
@@ -47,7 +47,7 @@ export async function createSharing(cardId: string, validHours = (24*7)) {
             return { error: 'user not found' };
         }
         const now = new Date();
-        const valid_until = now.setTime(now.getTime()+(validHours*60*60*1000));
+        const valid_until = now.setTime(now.getTime() + validHours * 60 * 60 * 1000);
 
         const sharing: ISharing = await SharingModel.create({ user, card, valid_until });
 
@@ -97,7 +97,11 @@ export async function fetchSharing(id: string) {
             return { error: 'Sharing not found' };
         }
 
-        const sharing = await SharingModel.findOne<ISharing>({ _id: parsedId, valid_until: { $gte: new Date() }, isComplete: false });
+        const sharing = await SharingModel.findOne<ISharing>({
+            _id: parsedId,
+            valid_until: { $gte: new Date() },
+            isComplete: false,
+        });
         if (sharing) {
             return {
                 sharing,
@@ -125,34 +129,42 @@ export async function executeSharing(id: string) {
         if (!sharing) {
             return { error: 'Sharing not found' };
         }
-        const { card } : {card:ICard} = await sharing.populate('card');
+        const { card }: { card: ICard } = await sharing.populate('card');
 
         const { types } = await getTypes();
         if (!types) {
             return { error: 'Unable to copy card' };
         }
-        const defaultType = types.filter(t => t.isDefault)[0];
+        const defaultType = types.filter((t) => t.isDefault)[0];
         if (!defaultType) {
             return { error: 'Unable to copy card' };
         }
-        const {card: newCard, error} = await createCard(new CardAdaptor({image_front: card.image_front, image_back: card.image_back, name: card.name, type_id: defaultType._id!, number: card.number, note: card.note}));
+        const { card: newCard, error } = await createCard(
+            new CardAdaptor({
+                image_front: card.image_front,
+                image_back: card.image_back,
+                name: card.name,
+                type_id: defaultType._id!,
+                number: card.number,
+                note: card.note,
+            })
+        );
         if (newCard) {
             sharing.isComplete = true;
             sharing.recipient = user;
             sharing.recieved_at = new Date();
             sharing.save();
             return {
-                card: newCard
-            }
+                card: newCard,
+            };
         } else {
             console.log(error);
-            return { error: 'Unable to copy card' }
+            return { error: 'Unable to copy card' };
         }
     } catch (error) {
         return { error };
     }
 }
-
 
 export async function deleteSharing(id: string) {
     try {
