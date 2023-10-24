eac871a681175792a084b5172c847a387f29b69f
Nasu Don <hck.hongkong@gmail.com>
2023-10-24 23:49:52 +0900

    KAADO-45: Fix style

--- src/app/sharing/[id]/page.tsx
+++ src/app/sharing/[id]/page.tsx
@@ -29,23 +29,21 @@ export default async function Page({ params }: { params: { id: string } }) {
     return (
         <div className="w-96 min-h-screen p-4 grid grid-rows-[auto_1fr] gap-10">
             <TopNav left={backBtn} />
-            <div className="w-full justify-self-center flex flex-col items-center gap-5">
-                <div className="w-10/12 flex flex-col items-center gap-1">
-                    <div className="text-xl text-center">
-                        {sharing.user.name} <br />
-                        has shared a card with you
-                    </div>
-                    <div className="w-36 h-24 overflow-hidden rounded-xl border">
-                        <Image
-                            src={sharing.card.image_front}
-                            alt="placeholder"
-                            className="w-full h-full object-cover object-center"
-                            width="476"
-                            height="300"
-                        />
-                    </div>
+            <div className="w-10/12 justify-self-center flex flex-col items-center gap-5">
+                <div className="text-xl text-center">
+                    {sharing.user.name} <br />
+                    has shared a card with you
                 </div>
-                <div className="w-10/12 mt-4">
+                <div className="w-36 h-24 overflow-hidden rounded-xl border">
+                    <Image
+                        src={sharing.card.image_front}
+                        alt="placeholder"
+                        className="w-full h-full object-cover object-center"
+                        width="476"
+                        height="300"
+                    />
+                </div>
+                <div className="w-full">
                     <AcceptButton title="Accept Card" handleAccept={handleAccept} />
                 </div>
             </div>
